8883bf53f6634e7c47939fda1b523671576b635f
pratik1509 <gajjarpratikn@gmail.com>
2025-01-07 20:03:52 +0530

    Added git ignore

--- Services/Catalog/Catalog.API/obj/Debug/net9.0/Catalog.API.AssemblyInfo.cs
+++ Services/Catalog/Catalog.API/obj/Debug/net9.0/Catalog.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3daca3b9f45aea01c3026254d3126403e423b088")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32567393762e40c1f7f0c5729518e4193ce8e612")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
